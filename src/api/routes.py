ea0cad37cb2b120ea8597a0d0397bdaf8f22a5b2
Manuel García <153007701+delafter@users.noreply.github.com>
2024-05-13 19:28:36 +0000

    MODELOS ECHO CORRECTAMENTE

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,10 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_cors import CORS 
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 from flask_cors import CORS
 
 api = Blueprint('api', __name__)
@@ -12,11 +16,32 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+@api.route('/signup', methods=['POST'])
+def handle_signup():
+    request_body = request.get_json()
+   
+    email = request_body.get('email')
+    password = request_body.get('password')
+  
+    user = User(email=email, password=password)
+    db.session.add(user)
+    db.session.commit()
+    reponse_body = {
+        'msg': 'User created successfully',
+        'user': user.serialize()
     }
 
-    return jsonify(response_body), 200
+
+    return jsonify(reponse_body), 200
+
+@api.route('/login', methods=['POST'])
+def handle_login():
+    request_body = request.get_json()
+    email = request_body.get('email')
+    password = request_body.get('password')
+    user = User.query.filter_by(email=email, password=password).first()
+    if user is None:
+        return jsonify({'msg': 'Error en el email o password'}), 401
+   
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token), 200
