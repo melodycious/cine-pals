c124c6603641a15a87f45f8490af1dcc741dbc16
Carla <152617155+Cl1493@users.noreply.github.com>
2024-05-15 18:10:39 +0000

    migraciones varias

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,8 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, List
+from sqlalchemy.orm import joinedload
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS 
 from flask_jwt_extended import create_access_token
@@ -47,31 +48,40 @@ def handle_login():
     return jsonify(access_token=access_token), 200
 
 
+@api.route('/newuser/<int:id>/<int:listid>', methods=['POST'])
+def handle_newuser(id,listid):
+    user = User.query.get(id)
+    targetList = List.query.get(listid)
+    user.lists.append(targetList)
+    db.session.commit()
+    return "Pleaseee!!"
+
 @api.route('/users/<int:id>', methods=['GET'])
 def handle_get_one_user(id):
-    user = User.query.get(id)
-    users = User.query.all()
-    users_serialized = []
-    for user in users:
-        users_serialized.append(user.serialize())
+    user = User.query.options(joinedload(User.lists).joinedload(List.movies),joinedload(User.lists).joinedload(List.series)).get(id)
+    
     response_body = {
-        "msg": "The user: ",
-        "user": user.serialize()
+        "id": user.id,
+        "name": user.nombre,
+        "email": user.email,
+        "lists": [{
+            "id": list.id,
+            "name": list.name,
+            "users": list.user_id,
+            "movies": [{
+                "id": movie.id
+            } for movie in list.movies],
+            "series": [{
+                "id": serie.id
+            } for serie in list.series]
+        } for list in user.lists]
     }
     return jsonify(response_body), 200
 
-""" @api.route('/users', methods=['GET'])
-def handle_get_users():
-    users = User.query.all()
-    users_serialized = []
-    for user in users:
-        users_serialized.append(user.serialize())
-    response_body = {
-        "msg": "The users: ",
-        "users": users_serialized
-    }
-    return jsonify(response_body), 200
- """
+
+
+
+
 @api.route('/users/<int:id>', methods=['DELETE'])
 def handle_delete_user(id):
     user = User.query.get(id)
