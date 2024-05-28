71e3c80999c40303a17f4ed6e9e740ffe28e5ea9
Carla <152617155+Cl1493@users.noreply.github.com>
2024-05-28 12:07:16 +0000

    Arreglado al recargar pagina sigue dentro del usuario. Se ve el modal de nueva lista

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -76,7 +76,8 @@ const getState = ({ getStore, getActions, setStore }) => {
         )
         .then((response) => response.json())
         .then((result) => {
-            setStore({ token: result.access_token, userId: result.userId });
+          const store = getStore()
+            setStore({...store, token: result.access_token, userId: result.userId });
             console.log('result: ' + JSON.stringify(result));
             // Llamar a getTraerUsuario justo después de obtener el token y el userId
             getActions().getTraerUsuario(result.userId);
@@ -91,8 +92,14 @@ const getState = ({ getStore, getActions, setStore }) => {
         console.log(token);
 
         setStore({ token: "" });
+        localStorage.clear();
         navigate("/");
 
+      },
+
+      setSession: (token,usuario) => {
+        const store = getStore()
+        setStore({...store, token, usuario:JSON.parse(usuario)})
       },
 
 
@@ -252,48 +259,6 @@ const getState = ({ getStore, getActions, setStore }) => {
       list.push(movieTitle);
       setStore({ listas: { ...store.listas, [list_id]: list } });
     },
-  
-/* 
-    getEditUser: async (id, nombre, email, password) => {
-      const myHeaders = new Headers();
-          myHeaders.append("Content-Type", "application/json");
-          myHeaders.append("Cookie", ".Tunnels.Relay.WebForwarding.Cookies=CfDJ8E0FHi1JCVNKrny-ARCYWxOnQLzo-WJT0sdNJZNG7IgDqzf0wx2UZHMzZR7RT1nhj0nL-YWwVG3-6OezD5YyZU-yGJ-oLH0l59d7K3yqi1qPUhFDQR7qemGeagONwwzHiwMP_J6ygdX3kyv-ZbLpkaXpKct2nK31pQ3W0CAe6E53vJgkkD5OwK8unsX01i9OoOpc8Nq3K2QiCJi2-v3dXCjiN9I_aIjFXasSUpWzfmNrY-wb3pnWOk50P-oB6YDI9sKg9rOSAA2saBKqE26Xdy2cLLEcOGx6JOpD61bEKvf2zyljqMFC8KKDzNUHDNYa7UoMCZfaasXiTmKiLpfRomvnyvSlDEKJU9CVf9FYTWnoXxUvUxT87NwcsCU4rcXK3iZTfH4D-y8HNKJmgs5b9xYX7ObkSceZ3fc3lLeTR5UbgFLpC_oAQzC83xZdyoi2CcvuDttZxvZlZp-BQeK914GtjsUMOX52Z11jYYt1K2VYT3kfE6neBVCYRsOUEph1hH1vUO9VrHCNXgo8CX41VFvG-AptW4VfGd_hMsMNDuwra20aE7BMRr_b2MFv5DkDtGOvR6pwxnFZe1pWlqPsl3RKRnJcQjzLZDp8AIbw-xjvr04pd7vakywW3VX57OtjrljsMwjDcfqNjZwfcbGGNcC834e-PWgP1RwDIJImHStWHosYE3i5G6139n5SFYLp6gTdpxU1trh1aVJk5K3ov0S4L6t4sXQDZmMJfB-JPyKFcrOmYKuyzkA6PjRyWDzpXQIQd_HOhyxUIiiHt3WF7PE93KOcJ3mlRvS1fxr662rf7i2PFAJaM5XnArnrEuN4qlMd9iki6lATuYU2omXlbVTidSjTx_eag_nyXEfJdysUTa4fMKExJqEzzRv0rYkRMSE-PhbjAHUxQBHisVdTcdJ_Fmf2tDRAHzZsgupUeI3BHwjuK-B4T4lzwAyi3ReDNPgXnImSfn9vLW_2VzDfa0X9JIvcT9d-ZUY8EUotGQGa");
-
-          const raw = JSON.stringify({
-            "email": email,
-            "password": password,
-            "name": nombre
-          });
-
-          const requestOptions = {
-            method: "PUT",
-            headers: myHeaders,
-            body: raw,
-            redirect: "follow"
-          };
-
-          fetch(`https://psychic-space-carnival-x55p5rqv6wvxc6gr7-3001.app.github.dev/api/users/${id}`, requestOptions)
-            .then((response) => response.text())
-            .then((result) => setStore({ usuario: result }))
-            .catch((error) => console.error(error));
-
-    },
-
-    getDeleteUser: async (id) => {
-      const myHeaders = new Headers();
-        myHeaders.append("Cookie", ".Tunnels.Relay.WebForwarding.Cookies=CfDJ8E0FHi1JCVNKrny-ARCYWxORm-9RVJfLsAAtN5lJiZYaB2XUzMRB5Zdr_Iw1KrO0yFWAzzt-zLjcTqenqh2VivhDRmQp-r_q1nVh0-RWYojbiNAXJEGXBiyrualJseX-5N-7Z9BSE_caxVtu8LtDBh7UVckO3dKZh_HSU21SrE5kSa5HW7jmJMM-Pk5_mlp_AZE8t5GOy9H1UfmXHxDBhx97xQMAq2jZIfaqQ0iQcQaabhUWFqdgX9qODzUAn9AkP-TGBnw38BVoEDOF2t1ZUeUoQ9zanuFZr14fnJACKrzpolhZjWPPkEtFokqEOqid2dgnqhFnUliVd-Bzdci3VyjYrG7EWDtqIcWTrxg8tb-wY1Re_hG_3XezLPeKf9ap9qicWZdAfGHOsJGUrFDPbOGSSX3e39M30UHlYyf7hphGlghcXAJqThQtBX1I8MA93TVyqEFi4UfZaoZHmecgecRyqRFisHtRVoKLLfg5DEgbRVDVSpUoJVaVZ9BBem-jRkXrDkUeTRyb7OwDLsiFFZ_bfHFTsH34qsWcuNsk5mRhx5drKK4qw4T37yo6fQRYUGWvPFqzGD647wT-Qhb8yvA5RoRC9ef1Vj7i6rY5dT6o6rzBEvL5Pm3AxLmAwSj2JyEjdWPefHeRDxabbDSV6kSfdjYFFijLRIyYTpU5oZAmsYiUQKU24g-epvrjqNJJaFp7f58MKYIFppIdkscG7oQHY15yvgOgJNbaJmPUv0SD3KlZtaZBoMIkt6Dqr4CjDKUt1ImlDbS8q1X--mIR3Z6Go04cl_qUnsiINVedkIgpEWOgUxxMxS6F87iBD1qNEfNLwELe9Uxh4teiocBCgSdqJmsi23Vsjq7JItFPAQSY2IyFHx4bDiT0OYKSjP8ZzNgNkGq894Nd_QxFgGVfh2U_tX_8cD6RaKA3uTYRrqBUWXhc0PYN8gJ_ps0umQWuClGFM1vm2F_s0WHpmwKpttqW-8z7M1vzTCvBEbwXM25P");
-
-        const requestOptions = {
-          method: "DELETE",
-          headers: myHeaders,
-          redirect: "follow"
-        };
-
-        fetch(`https://psychic-space-carnival-x55p5rqv6wvxc6gr7-3001.app.github.dev/api/users/${id}`, requestOptions)
-          .then((response) => response.text())
-          .then((result) => setStore({ usuario: result }))
-          .catch((error) => console.error(error));
-    }, */
 
 
 getTraerUsuario: async () => {
@@ -314,7 +279,7 @@ getTraerUsuario: async () => {
     
             const data = await response.json();
             console.log("Datos del usuario:", data);
-            setStore({ usuario: data });
+            setStore({...store, usuario: data });
         } catch (error) {
             console.error("Error al obtener el usuario", error);
         }
